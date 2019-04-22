a2a218886389af17619c265fc95a93ddfffa3454
KazumaUede <katsuodashi794@gmail.com>
2019-04-22 16:12:13 +0900

    人の色分け、人数計算

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(function(){
 	for (var i = 0; i < 6; i++){
-		$('#elevator'+ i +' #1F').css({"background-color" : "#f4d420"});
+		$('#elevator'+ i +' #F1').css({"background-color" : "#f4d420"});
 	}
 
 	var flug = true;
@@ -8,24 +8,33 @@ $(function(){
 	function up_elevator(num,floor,time,array){
 		var eletimer = setTimeout(function(){
 			clearTimeout(eletimer);
-			$('#elevator'+ num +' #' + floor +'F .people').appendTo('#elevator'+ num +' #' + (floor + 1) +'F')
-			$('#elevator'+ num +' #' + floor +'F').css({"background-color" : "#26499d"});
-			$('#elevator'+ num +' #' + ++floor +'F').css({"background-color" : "#f4d420"});
+			$('#elevator'+ num +' #F' + floor +' .people').appendTo('#elevator'+ num +' #F' + (floor + 1))
+			$('#elevator'+ num +' #F' + floor ).css({"background-color" : "#26499d"});
+			$('#elevator'+ num +' #F' + ++floor ).css({"background-color" : "#f4d420"});
 			//人を下ろす
 			if(array.find(item => item === floor)){
-				console.log('#people' + floor);
-				$('#elevator'+ num +' #' + floor +'F #people' + floor).remove();
-
+				//何人下ろすか数える
+				var count = array.filter(num => num == floor).length;
+				//降りる人数の要素を追加する
+				$('#elevator'+ num +' #F' + floor +' #people' + floor).remove();
+				for(var i = 0; i < count; i++){
+					$('#count' + floor).append("<p></p>");
+				}
+				//人数の情報を消す
+				$('#sum' + floor + ' p').remove();
+				$('#sum1 p').remove();
+				var sum = $('#count' + floor + ' p').length;
+				$('#sum' + floor).append('<p>' + sum +'人</p>');
+				var sumall = 0;
+				for(var i = 2; i < 14; i++){
+					sumall += $('#count' + i + ' p').length
+				}
+				$('#sum1').append('<p>' + sumall +'人</p>');
 			}
 			var timer = array.find(item => item === floor)? 5000 : 2000;
-			// if(floor <13){
-			// if(floor != array.slice(-1)[0]){
 			if(floor < array.reduce((a,b)=>a>b?a:b)){
-				// .reduce((a,b)=>a>b?a:b)
-				console.log(floor);
 				up_elevator(num,floor,timer,array);
 			}else{
-				console.log(floor);
 				down_elevator(num,floor,timer);
 			}
 		},time);
@@ -34,17 +43,12 @@ $(function(){
 	function down_elevator(num,floor,time){
 		var eletimer = setTimeout(function(){
 			clearTimeout(eletimer);
-			$('#elevator'+ num +' #' + floor +'F').css({"background-color" : "#26499d"});
-			$('#elevator'+ num +' #' + --floor +'F').css({"background-color" : "#f4d420"});
+			$('#elevator'+ num +' #F' + floor).css({"background-color" : "#26499d"});
+			$('#elevator'+ num +' #F' + --floor).css({"background-color" : "#f4d420"});
 			if(floor >1){
 				down_elevator(num,floor,2000);
 			}else{
 				setpeople(num,5000);
-				// var ary = [];
-				// ary = people();
-				// console.log(ary);
-				// notice_elevator(num,ary);
-				// up_elevator(num,1,5000,ary);
 			}
 		},time);
 	}
@@ -52,9 +56,8 @@ $(function(){
 	function notice_elevator(num,array){
 		for(var i = 1; i < 14; i++){
 			if(array.find(item => item === i)){
-				$('#elevator'+ num +' #' + i +'F').css({"background-color" : "#f18e10"});
+				$('#elevator'+ num +' #F' + i).css({"background-color" : "#f18e10"});
 			}
-
 		}
 	}
 
@@ -73,7 +76,7 @@ $(function(){
 			ary = people();
 			console.log(ary);
 			ary.forEach(function( value ) {
-				$('#elevator'+ num +'  #1F').append('<div class="people" id ="people'+ value +'"></div>');
+				$('#elevator'+ num +'  #F1').append('<div class="people" id ="people'+ value +'"></div>');
 			});
 			notice_elevator(num,ary);
 			up_elevator(num,1,time,ary);
@@ -81,19 +84,10 @@ $(function(){
 
 
 	$(function(){
+		//サンプル開始
 		for(var i = 0; i < 6; i++){
 			setpeople(i,2000);
 		}
-		// for(var i = 0; i < 6; i++){
-		// 	var ary = [];
-		// 	ary = people();
-		// 	console.log(ary);
-		// 	ary.forEach(function( value ) {
-		// 		$('#elevator'+ i +'  #1F').append('<div class="people" id ="people'+ value +'"></div>');
-		// 	});
-		// 	notice_elevator(i,ary);
-		// 	up_elevator(i,1,2000,ary);
-		// }
 	});
 });
 
--- template/building.php
+++ template/building.php
@@ -1,24 +1,36 @@
-<?php
-?>
-<div class ="building">
+<div class ="buildings">
 	<div class ="gates">
 		<?php
 			for($i = 0; $i <$floor ; ++$i){
-				echo '<div class ="gate" id ="' . ($floor - $i ) .'F"></div>';
+				// echo '<div class ="gate" id ="F' . ($floor - $i ) .'">'. ($floor - $i ) .'F</div>';
+				if (($floor - $i ) !==1){
+					echo '<div class ="gate" id ="F' . ($floor - $i ) .'">';
+						echo '<div class ="floorname" >'. ($floor - $i ) .'F</div>';
+						echo '<div class ="count" id ="count' . ($floor - $i ) .'"></div>';
+						echo '<div class ="sum" id ="sum' . ($floor - $i ) .'"></div>';
+					echo '</div>';
+				}else{
+					echo '<div class ="gate" id ="F' . ($floor - $i ) .'">';
+						echo '<div class ="floorname" >合計</div>';
+						echo '<div class ="sum" id ="sum' . ($floor - $i ) .'"></div>';
+					echo '</div>';
+				}
 			}
 			?>
 	</div>
-	<div class ="elevators">
-		<?php
-			for($i = 0; $i <$elevator ; ++$i){
-				echo '<div class ="elevator" id ="elevator'. $i .'">';
-				echo '<div class ="standby" id ="stand'. $i .'"></div>';
-				echo '<ul class="floors">';
-				for($j = 0; $j < $floor; ++$j){
-					echo '<li id ="' . ($floor - $j ) .'F"></li>';
+	<div class ="building">
+		<div class ="elevators">
+			<?php
+				for($i = 0; $i <$elevator ; ++$i){
+					echo '<div class ="elevator" id ="elevator'. $i .'">';
+					echo '<div class ="standby" id ="stand'. $i .'"></div>';
+					echo '<ul class="floors">';
+					for($j = 0; $j < $floor; ++$j){
+						echo '<li id ="F' . ($floor - $j ) .'"></li>';
+					}
+					echo '</ul></div>';
 				}
-				echo '</ul></div>';
-			}
-		?>
+			?>
+		</div>
 	</div>
 </div>
