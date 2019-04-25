862cab87832e174b315808cc42688a7c3abef531
KazumaUede <katsuodashi794@gmail.com>
2019-04-25 15:34:25 +0900

    競争

--- js/index.js
+++ js/index.js
@@ -6,15 +6,19 @@ $(function(){
 	var ary = [];
 	var priority =[];
 	var stand = [];
+	var stand2 = [];
 	var members = [];
 	var left = '.left ';
 	var right = '.right ';
+	var starttimer =[];
 	for (var i = 0; i < elevator * 2 ; i++){
 		flag.push(true);
 		ary.push([]);
 		priority.push(false);
+		stand2.push([]);
+		starttimer.push(0);
 	}
-	for (var i = 0; i < calcfloors; i++){
+	for (var i = 0; i < calcfloors * 2; i++){
 		members[i] = 0;
 	}
 
@@ -22,7 +26,7 @@ $(function(){
 	function up_elevator(num,floor,time,array,choices){
 		var eletimer = setTimeout(function(){
 			clearTimeout(eletimer);
-			var elenum = i < elevator ?num:num - 6;
+			var elenum = num < elevator ?num:num - 6;
 			$(choices +'.elevator:eq('+ elenum +') .floor:eq('+ floor +') .people').appendTo(choices +'.elevator:eq('+ elenum +') .floor:eq('+ (floor - 1) +')');
 			$(choices +'.elevator:eq('+ elenum +') .floor:eq('+ floor +')').css({"background-color" : "#26499d"});
 			$(choices +'.elevator:eq('+ elenum +') .floor:eq('+ --floor +')').css({"background-color" : "#f4d420"});
@@ -33,15 +37,28 @@ $(function(){
 				var count = array.filter(num => num == floor).length;
 				//降りる人数の要素を追加する
 				$(choices +'.elevator:eq('+ elenum +') .floor:eq('+ floor +') #people' + floor).remove();
-				members[floor] += count;
+				var choicefloor = num < elevator ?floor:floor + calcfloors ;
+				members[choicefloor] += count;
+				// members[floor] += count;
 				//人数の情報を消す
 				$(choices +'.gate:eq('+ floor +') .sum p').remove();
 				$(choices +'.gate:eq('+ calcfloors +') .sum p').remove();
-				$(choices +'.gate:eq('+ floor +') .sum').append('<p>' + members[floor] +'人</p>');
+				$(choices +'.gate:eq('+ floor +') .sum').append('<p>' + members[choicefloor] +'人</p>');
+				// $(choices +'.gate:eq('+ floor +') .sum').append('<p>' + members[floor] +'人</p>');
 				var sumall = 0;
-				for(var i = 0; i < calcfloors; i++){
+				if (num < elevator){
+					var start = 0;
+					var end = calcfloors;
+				}else{
+					var start = calcfloors;
+					var end = calcfloors * 2;
+				}
+				for(var i = start; i < end; i++){
 					sumall += members[i];
 				}
+				// for(var i = 0; i < calcfloors; i++){
+				// 	sumall += members[i];
+				// }
 				$(choices +'.gate:eq('+ calcfloors +') .sum').append('<p>' + sumall +'人</p>');
 			}
 			var timer = array.find(item => item === floor) || floor === 0? 10000 : 2000;
@@ -63,16 +80,16 @@ $(function(){
 			if(floor < calcfloors){
 				down_elevator(num,floor,2000,choices);
 			}else{
+				var waitary =[];
 				if(num < elevator){
-
-					var waitary =[];
+					// var waitary =[];
 					while(stand.length > 0){
 						waitary.push(stand.shift());
 						if(waitary.length >= 20){
 							break;
 						}
 					}
-					console.log(num +'番は'+ waitary.length +'人乗るぞ');
+					// console.log(num +'番は'+ waitary.length +'人乗るぞ');
 					$(choices +'.elevator:eq('+ elenum +') .floor:eq(' + calcfloors + ')').append($(choices +'.standby:eq(0) .people:lt(' + waitary.length +')'));
 					ary[num] = waitary;
 					// console.log(ary[num]);
@@ -85,6 +102,24 @@ $(function(){
 						standby(num,flag[num],ary[num],choices);
 					}
 
+				}else{
+					while(stand2[num].length > 0){
+						waitary.push(stand2[num].shift());
+						if(waitary.length >= 20){
+							break;
+						}
+					}
+					// console.log(num +'番は'+ waitary.length +'人乗るぞ');
+					$(choices +'.elevator:eq('+ elenum +') .floor:eq(' + calcfloors + ')').append($(choices +'.standby:eq(' + elenum +') .people:lt(' + waitary.length +')'));
+					ary[num] = waitary;
+					if (ary[num].length >=20){
+						notice_elevator(num,ary[num],choices);
+						up_elevator(num,calcfloors,10000,ary[num],choices);
+					}else{
+						priority[num] = ary[num].length > 0? true:false;
+						flag[num] = true;
+						standby(num,flag[num],ary[num],choices);
+					}
 				}
 
 
@@ -105,6 +140,7 @@ $(function(){
 				setary.push(a);
 				var elenum = i < elevator ?num:num - 6;
 				$(choices +'.elevator:eq(' + elenum +') .floor').eq(calcfloors).append('<div class="people" id ="people'+ a +'"></div>');
+				starttimer[num] = 0;
 				flag[num] = setary.length >=20? false:true;
 	}
 
@@ -115,6 +151,7 @@ $(function(){
 			clearTimeout(timer);
 			var min = 0 ;
 			var max = 11 ;
+			// var max = 5 ;
 			var a = Math.floor( Math.random() * (max + 1 - min) ) + min;
 
 
@@ -144,31 +181,40 @@ $(function(){
 			//振り分ける(右用、近い階同士を集める)
 			switch(a){
 				case 0:
-				case 1: flag[6] ?		enter_the_elevator(6,a,right) :$(right +'.standby').eq(0).append('<div class="people" id ="people'+ a +'"></div>'); break;
+				case 1: var elenum = elevator; break;
 				case 2:
-				case 3: flag[7] ?		enter_the_elevator(7,a,right) :$(right +'.standby').eq(1).append('<div class="people" id ="people'+ a +'"></div>'); break;
+				case 3: var elenum = elevator + 1; break;
 				case 4:
-				case 5: flag[8] ?		enter_the_elevator(8,a,right) :$(right +'.standby').eq(2).append('<div class="people" id ="people'+ a +'"></div>'); break;
+				case 5: var elenum = elevator + 2; break;
 				case 6:
-				case 7: flag[9] ?		enter_the_elevator(9,a,right) :$(right +'.standby').eq(3).append('<div class="people" id ="people'+ a +'"></div>'); break;
+				case 7: var elenum = elevator + 3; break;
 				case 8:
-				case 9: flag[10] ?		enter_the_elevator(10,a,right) :$(right +'.standby').eq(4).append('<div class="people" id ="people'+ a +'"></div>'); break;
+				case 9: var elenum = elevator + 4; break;
 				case 10:
-				case 11: flag[11] ?	enter_the_elevator(11,a,right) :$(right +'.standby').eq(5).append('<div class="people" id ="people'+ a +'"></div>'); break;
+				case 11: var elenum = elevator + 5; break;
 			}
-
-
-
-
-
-
-
-
-
-
-
-			people2();
-		},1000);
+			if (flag[elenum]){
+				enter_the_elevator(elenum,a,right);
+			}else{
+				$(right +'.standby').eq(elenum - elevator).append('<div class="people" id ="people'+ a +'"></div>');
+				var setary = stand2[elenum];
+				setary.push(a);
+			}
+			var standall =0;
+			for(var i = elevator; i < elevator * 2;i++){
+				var j = stand2[i]
+				standall+= j.length;
+			}
+			if(stand.length >=500){
+				$('.ground').append('<div class="result">左ビルキャパオーバー!!右ビルの勝ち！！</div>');
+				return false;
+			}else if(standall >=500){
+				$('.ground').append('<div class="result">右ビルキャパオーバー!!左ビルの勝ち！！</div>');
+				return false;
+			}else{
+				people2();
+			}
+		},500);
 	}
 
 
@@ -176,13 +222,16 @@ $(function(){
 	function standby(num,flg,standary,choices){
 		var timer = setTimeout(function(){
 			clearTimeout(timer);
-			if( standary.length >= 20 && flg === false){
+			if( (standary.length >= 20 && flg === false) || ( standary.length >= 1 && starttimer[num] >= 10)){
+				flag[num] = false;
 				priority[num] = false;
+				starttimer[num] = 0;
 
-				console.log("エレベーター"+ num + "出発");
+				// console.log("エレベーター"+ num + "出発");
 				notice_elevator(num,standary,choices);
 				up_elevator(num,calcfloors,2000,standary,choices);
 			}else{
+				starttimer[num] += 1;
 				standby(num,flag[num],standary,choices);
 			}
 		},1000);
@@ -222,7 +271,7 @@ $(function(){
 			// $('tr').remove();
 			$('.routes').remove();
 			var appendhtml;
-			console.log(response);
+			// console.log(response);
 			var station_name;
 			appendhtml = '<div class="routes">'  + response + '</div>';
 			$('.result').append(appendhtml)
